e6f7c76651ff24583547436cd4a53dcbf0125868
13KuntalRoy <roykuntal1213@gmail.com>
2021-08-25 23:45:00 +0530

    last time add

--- projecto1/todo.js
+++ projecto1/todo.js
@@ -10,13 +10,41 @@
 //     val--;
 // }
  var ul =document.getElementById('list')
+ console.log(ul);
  var li
 const addItem=()=>{
-    console.log('add button clicked');
+   var input=document.getElementById('input')
+   var item = input.value
+   ul = document.getElementById('list')
+   var textnode = document.createTextNode(item)
+
+   if(item === '')
+   {
+       return false;
+   }
+   else{
+
+
+       li = document.createElement('li')
+       var checkbox = document.createElement('input')
+       checkbox.type='checkbox';
+       checkbox.setAttribute('id','check')
+
+       var label =document.createElement('lable')
+       label.setAttribute('for','item')
+
+       ul.appendChild(label)
+       li.appendChild(checkbox)
+       label.appendChild(textnode)
+       li.appendChild(label)
+       ul.insertBefore(li,ul.childNodes[0])
+       li.className='visual'
+   }
 }
 
 const removeItem=()=>{
     li=ul.children
+    console.log(li);
     for(let index =0; index <li.length; index++)
     {
      while(li[index].children[0].checked)
@@ -30,6 +58,7 @@ const removeItem=()=>{
 
 const removeallItem=()=>{
     li=ul.children
+    console.log(li);
     for(let index =0; index <li.length; index++)
     {
 
